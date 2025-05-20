d0ec4bc9ec00926c15c776e52d285813da79af31
David Hoppe <dh@smpmedia.net>
2025-05-20 10:15:46 +0200

    Create game if no image selected

--- public/create-game.php
+++ public/create-game.php
@@ -1,22 +1,24 @@
 <?php
 require_once __DIR__ . '/../src/db.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['image_ids']) && is_array($_POST['image_ids'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $pdo->beginTransaction();
 
     $stmt = $pdo->prepare("INSERT INTO games (user_id, date, result) VALUES (1, DATE('now'), 0)");
     $stmt->execute();
     $gameId = $pdo->lastInsertId();
 
-    $imageIds = $_POST['image_ids'];
-    foreach ($imageIds as $pos => $id) {
-        $stmt = $pdo->prepare("UPDATE images SET game_id = ?, position = ? WHERE id = ?");
-        $stmt->execute([$gameId, $pos + 1, $id]);
+    if (isset($_POST['image_ids']) && is_array($_POST['image_ids'])) {
+        $imageIds = $_POST['image_ids'];
+        foreach ($imageIds as $pos => $id) {
+            $stmt = $pdo->prepare("UPDATE images SET game_id = ?, position = ? WHERE id = ?");
+            $stmt->execute([$gameId, $pos + 1, $id]);
+        }
     }
 
     $pdo->commit();
     header("Location: /edit-game.php?id=" . $gameId);
     exit;
 } else {
-    echo "No images selected.";
+    echo "No game created.";
 }
